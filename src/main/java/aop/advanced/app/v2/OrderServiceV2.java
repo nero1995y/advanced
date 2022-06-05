17b6963c6b10da0f4f3a5eaa483e301ebc7dcd83
nero <wnsgur765z@naver.com>
2022-06-05 23:13:23 +0900

    feat: Add v2 로그추적기 애플리케이션 적용

---
+++ src/main/java/aop/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package aop.advanced.app.v2;
+
+import aop.advanced.trace.TraceId;
+import aop.advanced.trace.TraceStatus;
+import aop.advanced.trace.hellotrace.HelloTraceV1;
+import aop.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId,"OrderService.request()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+
+}
